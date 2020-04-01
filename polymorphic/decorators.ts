48e71d9b9d07f5456a29fad5fcdf92971b0fbadd
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2020-04-01 17:08:19 +0100

    created decorators

---
+++ polymorphic/decorators.ts
@@ -0,0 +1,49 @@
+import { POLYMORPHIC_OPTIONS } from './contstants';
+import {
+  PolymorphicChildrenDecoratorOptionsInterface,
+  PolymorphicParentDecoratorOptionsInterface,
+  PolymorphicMetadataOptionsInterface,
+} from './polymorphic.interface';
+
+const polymorphicPropertyDecorator = (
+  options: PolymorphicMetadataOptionsInterface,
+): PropertyDecorator => (target: Object, propertyKey: string) => {
+  Reflect.defineMetadata(POLYMORPHIC_OPTIONS, true, target);
+  Reflect.defineMetadata(
+    `${POLYMORPHIC_OPTIONS}::${propertyKey}`,
+    {
+      propertyKey,
+      ...options,
+    },
+    target,
+  );
+};
+
+export const PolymorphicChildren = (
+  classType: () => Function[] | Function,
+  options: PolymorphicChildrenDecoratorOptionsInterface = {},
+): PropertyDecorator =>
+  polymorphicPropertyDecorator({
+    type: 'children',
+    classType,
+    hasMany: true,
+    eager: true,
+    cascade: true,
+    deleteBeforeUpdate: false,
+    ...options,
+  });
+
+export const PolymorphicParent = (
+  classType: () => Function[] | Function,
+  options: PolymorphicParentDecoratorOptionsInterface = {},
+): PropertyDecorator =>
+  polymorphicPropertyDecorator({
+    type: 'parent',
+    classType,
+    hasMany: false,
+    eager: true,
+    cascade: true,
+    deleteBeforeUpdate: false,
+    ...options,
+  });
+  
