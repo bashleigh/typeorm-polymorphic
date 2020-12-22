78b8e9854b6d35c8c5bc99904bf7743dbde2c383
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2020-12-22 17:27:45 +0000

    feat: added save test

--- src/__tests__/polymorphic.repository.spec.ts
+++ src/__tests__/polymorphic.repository.spec.ts
@@ -1,12 +1,17 @@
-import { Connection, createConnection } from 'typeorm';
+import { Connection, createConnection, Repository } from 'typeorm';
 import { AdvertEntity } from './entities/advert.entity';
 import { UserEntity } from './entities/user.entity';
 import { config } from 'dotenv';
 import { resolve } from 'path';
 import { AdvertRepository } from './repository/advert.repository';
+import { AbstractPolymorphicRepository } from '../../dist';
 
 describe('AbstractPolymorphicRepository', () => {
   let connection: Connection;
+
+  let userRepository: Repository<UserEntity>;
+  let repository: AbstractPolymorphicRepository<AdvertEntity>;
+
   beforeAll(async () => {
     config({
       path: resolve(__dirname, '.', '..', '..', '.env'),
@@ -26,18 +31,46 @@ describe('AbstractPolymorphicRepository', () => {
 
   afterAll(async () => {
     await connection.close();
+
+    await userRepository.createQueryBuilder().delete().execute();
+    await repository.createQueryBuilder().delete().execute();
+
+    await Promise.all([
+      userRepository.createQueryBuilder().delete().execute(),
+      repository.createQueryBuilder().delete().execute(),
+    ]);
   });
 
-  it('Can create', async () => {
-    const repository = connection.getCustomRepository(AdvertRepository);
-
-    const user = new UserEntity();
-
-    const result = repository.create({
-      owner: user,
+  describe("child", () => {
+    it('Can create with parent', async () => {
+      const repository = connection.getCustomRepository(AdvertRepository);
+  
+      const user = new UserEntity();
+  
+      const result = repository.create({
+        owner: user,
+      });
+  
+      expect(result).toBeInstanceOf(AdvertEntity);
+      expect(result.owner).toBeInstanceOf(UserEntity);
     });
 
-    expect(result).toBeInstanceOf(AdvertEntity);
-    expect(result.owner).toBeInstanceOf(UserEntity);
+    it('Can save cascade parent', async () => {
+      const repository = connection.getCustomRepository(AdvertRepository);
+      const userRepository = connection.getRepository(UserEntity);
+  
+      const user = await userRepository.save(new UserEntity);
+  
+      const result = await repository.save(repository.create({
+        owner: user,
+      }));
+  
+      expect(result).toBeInstanceOf(AdvertEntity);
+      expect(result.owner).toBeInstanceOf(UserEntity);
+      expect(result.id).toBeTruthy();
+      expect(result.owner.id).toBeTruthy();
+      expect(result.entityType).toBe(UserEntity.name);
+      expect(result.entityId).toBe(result.owner.id);
+    });
   });
 });
