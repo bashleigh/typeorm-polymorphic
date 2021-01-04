54d9a4990e28845118dfa1fa89b8c0552d96110e
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2021-01-04 16:30:20 +0000

    chore: readded removed file, needed for other exception

---
+++ src/repository.token.exception.ts
@@ -0,0 +1,13 @@
+export class RepositoryTokenNotFoundException extends Error {	
+  constructor(classType: string) {	
+    super(	
+      `Repository token cannot be found for given classType [${classType}]`,	
+    );	
+  }	
+}	
+
+export class RepositoryNotFoundException extends Error {	
+  constructor(token: Function | string) {	
+    super(`Repository cannot be found for given token [${token}]`);	
+  }	
+}
