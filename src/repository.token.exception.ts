567cb8588a35024b2f129794c06664ba3afed1b3
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2021-01-04 16:31:27 +0000

    chore: formatting

--- src/repository.token.exception.ts
+++ src/repository.token.exception.ts
@@ -1,13 +1,13 @@
-export class RepositoryTokenNotFoundException extends Error {	
-  constructor(classType: string) {	
-    super(	
-      `Repository token cannot be found for given classType [${classType}]`,	
-    );	
-  }	
-}	
+export class RepositoryTokenNotFoundException extends Error {
+  constructor(classType: string) {
+    super(
+      `Repository token cannot be found for given classType [${classType}]`,
+    );
+  }
+}
 
-export class RepositoryNotFoundException extends Error {	
-  constructor(token: Function | string) {	
-    super(`Repository cannot be found for given token [${token}]`);	
-  }	
+export class RepositoryNotFoundException extends Error {
+  constructor(token: Function | string) {
+    super(`Repository cannot be found for given token [${token}]`);
+  }
 }
