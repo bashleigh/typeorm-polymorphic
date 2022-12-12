1f6e0173018c47111f9af9d52c9bc2acc7527bdf
dominicbachmann <dominic.bachmann4@gmail.com>
2022-12-12 21:21:28 +0100

    Remove old custom repositories from tests

--- src/__tests__/repository/advert.repository.ts
+++
@@ -1,6 +0,0 @@
-import { EntityRepository } from 'typeorm';
-import { AbstractPolymorphicRepository } from '../../polymorphic.repository';
-import { AdvertEntity } from '../entities/advert.entity';
-
-@EntityRepository(AdvertEntity)
-export class AdvertRepository extends AbstractPolymorphicRepository<AdvertEntity> {}
